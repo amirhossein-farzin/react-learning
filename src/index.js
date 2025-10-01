95f643d2ff53c11e5363f39465c6fab81f4ff8d2
amir <amirfarzinf80@gmail.com>
2025-10-01 20:43:07 +0330

    component with class 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Timer extends React.Component {
+  render() {
+    return <h2>it is {new Date().toLocaleTimeString()}</h2>;
+  }
+}
+
+class Hello extends React.Component {
+  render() {
+    return <h1>Hello World!</h1>;
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Hello/>
+        <Timer />
+      </div>
+    );
+  }
+}
+
+const tick = () => {
+  createRoot(root).render(<App />);
+};
+setInterval(() => {
+  tick();
+}, 1000);
