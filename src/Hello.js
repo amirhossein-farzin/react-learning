cbbd722e7bd4f80992b5630b6b7b3660ad257c4d
amir <amirfarzinf80@gmail.com>
2025-10-02 16:58:07 +0330

    "divide components"

---
+++ src/Hello.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
+
+class Hello extends React.Component {
+  render() {
+    return <h1>Hello World!</h1>;
+  }
+}
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -1,45 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./style.css";
+import App from "./App";
+
 const root = document.getElementById("root");
 
-class Timer extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      time: new Date().toLocaleTimeString(),
-    };
-  }
-  render() {
-    setInterval(() => {
-      this.setState({
-        time: new Date().toLocaleTimeString()
-      });
-    }, 1000);
-    return <h2 className="timer">it is {this.state.time}</h2>;
-  }
-}
-
-class Hello extends React.Component {
-  render() {
-    return <h1>Hello World!</h1>;
-  }
-}
-
-class App extends React.Component {
-  render() {
-    return (
-      <div className="main">
-        <Hello />
-        <Timer />
-      </div>
-    );
-  }
-}
-
-// const tick = () => {
 createRoot(root).render(<App />);
-// };
-// setInterval(() => {
-//   tick();
-// }, 1000);
+
