06f5ce61c59f83071d13fdc3a4f5f74fc1aa9d92
amir <amirfarzinf80@gmail.com>
2025-10-16 17:38:34 +0330

    " Delete, check, and X buttons "

--- To do list/src/TaskItems.jsx
+++ To do list/src/TaskItems.jsx
@@ -2,29 +2,50 @@ import React, { Fragment, useContext } from "react";
 import { TaskContext } from "./TaskContext";
 const TaskItems = () => {
   const { taskItems, setTaskItems } = useContext(TaskContext);
-  console.log(TaskItems);
-  return (
-    <Fragment>
-      <ul className="list-group m-0 p-0 mt-2">
-        {taskItems.map((t) => (
-          <li
-            className={`list-group-item d-flex justify-content-between ${
-              t.done ? "list-group-item-success" : ""
-            }`}
-          >
-            {t.title}
-            <span>
-              {t.done ? (
-                <i className="mx-1 me-3 pointer text-warning transition-200 text-hover-shadow fas fa-times"></i>
-              ) : (
-                <i className="mx-1 me-3 pointer text-success transition-200 text-hover-shadow fas fa-check"></i>
-              )}
-              <i className="mx-1 me-3 pointer text-danger transition-200 text-hover-shadow fas fa-trash"></i>
-            </span>
-          </li>
-        ))}
-      </ul>
-    </Fragment>
-  );
+
+  if (taskItems.length) {
+    const handleSetDoneTask = (id) => {
+      const index = taskItems.findIndex((t) => t.id === id);
+      let newTaskItems = [...taskItems]; //! Copying taskItems and puting that in this variable
+      newTaskItems[index].done = !newTaskItems[index].done;
+      setTaskItems(newTaskItems); //! Paste the copy of tasksItems
+    };
+
+    const handleDeleteTask = (id) => {
+      let newTasks = taskItems.filter((t) => t.id !== id);
+      setTaskItems(newTasks);
+    };
+
+    return (
+      <Fragment>
+        <ul className="list-group m-0 p-0 mt-2">
+          {taskItems.map((t) => (
+            <li
+              className={`list-group-item d-flex justify-content-between ${
+                t.done ? "list-group-item-success" : ""
+              }`}
+            >
+              {t.title}
+              <span>
+                <i
+                  className={`mx-1 me-3 pointer text-warning transition-200 text-hover-shadow fas fa-${
+                    t.done ? "times" : "check"
+                  }`}
+                  onClick={() => handleSetDoneTask(t.id)} //! using function in onClick method because, we want to get that an element => t.id
+                ></i>
+
+                <i
+                  className="mx-1 me-3 pointer text-danger transition-200 text-hover-shadow fas fa-trash"
+                  onClick={() => handleDeleteTask(t.id)}
+                ></i>
+              </span>
+            </li>
+          ))}
+        </ul>
+      </Fragment>
+    );
+  } else {
+    return <h4 className="text-center text-warning">هیچ کاری ثبت نشده❗</h4>;
+  }
 };
 export default TaskItems;
--- To do list/src/Topform.jsx
+++ To do list/src/Topform.jsx
@@ -1,13 +1,39 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext, useState } from "react";
+import { TaskContext } from "./TaskContext";
 
 const TopForm = () => {
+  const [task, setTask] = useState("");
+  const { taskItems, setTaskItems } = useContext(TaskContext);
+
+  const handleSetTask = (event) => {
+    //! input value
+    setTask(event.target.value);
+  };
+  const handleAddTask = (event) => {
+    //! add task
+    event.preventDefault();
+    setTaskItems([
+      ...taskItems,
+      { id: Math.random(), title: task, done: false },
+    ]); //! taskItems is Array / Pasting the taskItems and new item (the items have id, title, and done )
+    setTask(""); //! remove input's value
+  };
   return (
     <Fragment>
       <h4 className="text-center text-info text-shadow"></h4>
       <form>
         <div className="form-group d-flex">
-          <input type="text" className="form-control" />
-          <button type="submit" className="btn btn-success me-1">
+          <input
+            type="text"
+            className="form-control"
+            value={task}
+            onChange={handleSetTask}
+          />
+          <button
+            type="submit"
+            className="btn btn-success me-1"
+            onClick={handleAddTask}
+          >
             ثبت
           </button>
         </div>
